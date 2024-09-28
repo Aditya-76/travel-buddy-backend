07ede2f2f18de5a8aaed52a1f03b0a45e8758565
Aditya Shekhar <aditya7682@gmail.com>
2024-09-29 00:06:43 +0530

    first commit

---
+++ constants.ts
@@ -0,0 +1,7 @@
+export enum WebSocketActions {
+  JOIN_ROOM = "join_room",
+  SET_ROUTE = "set_route",
+  UPDATE_LOCATION = "update_location",
+  ROUTE_UPDATED = "route_updated",
+  LOCATION_UPDATED = "location_updated",
+}
